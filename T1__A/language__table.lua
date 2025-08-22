ed4dd255158ea1dc5d1bb511a9f96c8ab8dc7196
No Name.. <bloskmango@gmail.com>
2025-08-22 09:45:43 +0300

    Create language__table.lua

---
+++ T1__A/language__table.lua
@@ -0,0 +1,35 @@
+local EnglishToArabic = {
+    ["Home"] = "الرئيسية",
+    ["Main"] = "الأساسية",
+    ["Setting"] = "الاعدادات",
+    ["Reach V1"] = "الريتش V1",
+    ["Enable Reach"] = "تفعيل الريتش",
+    ["Click to activate the reach"] = "أضغط لتفعيل الريتش",
+    ["Language"] = "اللغة",
+    ["Welcome :"] = "مرحبا :",
+    ["Join Our Telegram Community"] = "انضم الى مجتمعنا عبر تيلجرام",
+    ["Click to Copy"] = "أضغط للنسخ",
+    ["Set"] = "تعين",
+    ["Click to apply"] = "أضغط للتطبيق"
+}
+
+local ArabicToEnglish = {
+    ["الرئيسية"] = "Home",
+    ["الأساسية"] = "Main",
+    ["الاعدادات"] = "Setting",
+    ["الريتش V1"] = "Reach V1",
+    ["تفعيل الريتش"] = "Enable Reach",
+    ["أضغط لتفعيل الريتش"] = "Click to activate the reach",
+    ["اللغة"] = "Language",
+    ["تغيير لغة الواجهة"] = "Change UI language",
+    ["مرحبا :"] = "Welcome :",
+    ["انضم الى مجتمعنا عبر تيلجرام"] = "Join Our Telegram Community",
+    ["أضغط للنسخ"] = "Click to Copy",
+    ["تعين"] = "Set",
+    ["أضغط للتطبيق"] = "Click to apply"
+}
+
+return {
+    EnglishToArabic = EnglishToArabic,
+    ArabicToEnglish = ArabicToEnglish
+}
